bfe8654def320c3c82c5bf284250324d9d07c970
Ben Mabey <ben@benmabey.com>
2009-08-24 10:50:58 -0600

    loop and requery twitter.  Also, send the Slide locals to render.

--- lib/codenote/dynamic_slides/twitter_quiz.rb
+++ lib/codenote/dynamic_slides/twitter_quiz.rb
@@ -7,8 +7,14 @@ class TwitterQuiz
 
   def update(slide)
     client = TwitterSearch::Client.new('codenote')
-    tweets = client.query(@query)
-    slide.update_with(tweets.last)
+    tweets = []
+
+    while tweets.empty?
+      tweets = client.query(@query)
+      Kernel.sleep 0.5 if tweets.empty?
+    end
+
+    slide.update_with(:locals => {:tweet => tweets.last})
   end
 
 end
--- spec/codenote/dynamic_slides/twitter_quiz_spec.rb
+++ spec/codenote/dynamic_slides/twitter_quiz_spec.rb
@@ -4,6 +4,7 @@ require 'faketwitter'
 
 describe TwitterQuiz do
   describe '#update' do
+    before(:each) { @slide = mock('slide', :update_with => nil) }
     after(:each) { FakeTwitter.reset }
 
     it "updates the slide with the first tweet that matches the provided query" do
@@ -14,14 +15,40 @@ describe TwitterQuiz do
       ])
 
       # expect
-      slide = mock('slide')
-      slide.should_receive(:update_with).with do |tweet|
-        tweet.text.should == 'cheese is very good'
+      @slide.should_receive(:update_with).with do |render_options|
+        render_options[:locals][:tweet].text.should == 'cheese is very good'
       end
 
       # when
-      TwitterQuiz.new('cheese').update(slide)
+      TwitterQuiz.new('cheese').update(@slide)
     end
 
+    it "keeps searching twitter until a tweet matches" do
+      # given
+      FakeTwitter.register_searches('cheese', [
+        {:results => []},
+        {:results => []},
+        {:results => [{:text => 'pepper jack cheese is sooo good'}]}
+      ])
+      # expect
+      @slide.should_receive(:update_with).with do |render_options|
+        render_options[:locals][:tweet].text.should == 'pepper jack cheese is sooo good'
+      end
+      # when
+      TwitterQuiz.new('cheese').update(@slide)
+    end
+
+    it "sleeps each time before it requeries twitter" do
+       # given
+      FakeTwitter.register_searches('cheese', [
+        {:results => []},
+        {:results => []},
+        {:results => [{:text => 'just tweetin about cheese'}]}
+      ])
+      # expect
+      Kernel.should_receive(:sleep).with(0.5).twice
+      # when
+      TwitterQuiz.new('cheese').update(@slide)
+    end
   end
 end
